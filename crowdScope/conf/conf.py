9727f911a6334b091f81fc488a8d1bd86c060bc6
WikiGenius <muhammedelyamani92@gmail.com>
2023-03-25 13:39:11 +0200

    update

--- crowdScope/conf/conf.py
+++ crowdScope/conf/conf.py
@@ -10,17 +10,14 @@ from ultralytics import YOLO
 model_path = 'assets/weights/bestv4_n_640.pt'
 # model_path = 'assets/weights/bestv5_n_640.pt'
 
-detector = asone.ASOne(detector=asone.YOLOV8N_PYTORCH,weights=model_path ,use_cuda=True)
+detector = asone.ASOne(detector=asone.YOLOV8N_PYTORCH, weights=model_path ,use_cuda=True)
+# tracker = asone.ASOne(detector=asone.YOLOV8N_PYTORCH,tracker=asone.BYTETRACK ,weights=model_path ,use_cuda=True)
 
 
 ageProto="assets/weights/age_deploy.prototxt"
 ageModel="assets/weights/age_net.caffemodel"
 
-genderProto="assets/weights/gender_deploy.prototxt"
-genderModel="assets/weights/gender_net.caffemodel"
-
 ageNet=cv2.dnn.readNet(ageModel,ageProto)
-genderNet=cv2.dnn.readNet(genderModel,genderProto)
 
 imgsz = int(model_path.split('.')[0].split('_')[-1])
 
@@ -35,7 +32,6 @@ gender_model = YOLO(gender_model_path)  # load a pretrained YOLOv8n classificati
 
 MODEL_MEAN_VALUES=(78.4263377603, 87.7689143744, 114.895847746)
 ageList=['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
-genderList=['Male','Female']
 
 GENDER_DICT={0: 'F', 1: 'M'}
 
